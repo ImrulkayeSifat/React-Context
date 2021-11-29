cb28ccfc1571eab3f95e909fab0c0a0767a4ec1e
ImrulkayeSifat <kayesimrul224@gmail.com>
2021-11-29 15:06:49 +0600

    useContext

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React,{useContext} from "react";
 import { ThemeContext } from '../contexts/ThemeContext';
-class BookList extends React.Component {
-    static contextType = ThemeContext;
-    render() { 
-        const {isLightTheme,light,dark} = this.context;
-        const theme = isLightTheme ? light : dark;
+// class BookList extends React.Component {
+//     static contextType = ThemeContext;
+//     render() { 
+//         const {isLightTheme,light,dark} = this.context;
+//         const theme = isLightTheme ? light : dark;
+//         return (
+//         <div className="book-list" style={{background:theme.bg,color:theme.syntax}}>
+//             <ul>
+//                 <li style={{background:theme.ui}}>the way of kings</li>
+//                 <li style={{background:theme.ui}}>the name of the wind</li>
+//                 <li style={{background:theme.ui}}>the final empire</li>
+//             </ul>
+//         </div>);
+//     }
+// }
+ 
+const BookList = () => {
+    const {isLightTheme,light,dark} = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
         return (
-        <div className="book-list" style={{background:theme.bg,color:theme.syntax}}>
-            <ul>
-                <li style={{background:theme.ui}}>the way of kings</li>
-                <li style={{background:theme.ui}}>the name of the wind</li>
-                <li style={{background:theme.ui}}>the final empire</li>
-            </ul>
-        </div>);
+            <div className="book-list" style={{background:theme.bg,color:theme.syntax}}>
+                <ul>
+                    <li style={{background:theme.ui}}>the way of kings</li>
+                    <li style={{background:theme.ui}}>the name of the wind</li>
+                    <li style={{background:theme.ui}}>the final empire</li>
+                </ul>
+            </div>
+        );
     }
-}
+
  
 export default BookList;
+
